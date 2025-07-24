f1db8dc9e55dc5283da4659f65eca3fe38572cc0
MADHUNIKA DANAM <danammadhunika@gmail.com>
2025-07-24 14:52:55 -0400

    Add files via upload

---
+++ Chapter 2/07_pr_avg.py
@@ -0,0 +1,7 @@
+# Write a Python program to find average of two numbers entered by the user
+a = input("Enter first number")
+b = input("Enter second number")
+a = int(a)
+b = int(b)
+avg = (a + b)/2
+print("The average of a and b is", avg)
---
+++ Chapter 2/08_pr_calculate_square.py
@@ -0,0 +1,10 @@
+# Write a python program to caluculate square of a number entered by the user
+a = input("Enter the number")
+a = int(a)
+square = a * a
+print("The square of a is", square)
+
+
+
+
+
